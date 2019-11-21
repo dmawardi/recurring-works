b85943d8c76b8c42627572f98d029c5362ea879e
Dannar <dmawardi@gmail.com>
2019-11-21 13:17:52 +1100

    npm run build completed

---
+++ client/build/precache-manifest.b713c8791c1df9d5175d7acc09e933dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7482cd7cfe8879bd0d9ff9d27a7ea021",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a19a1f3f6c6a9714acf0",
+    "url": "/static/css/main.9925ac19.chunk.css"
+  },
+  {
+    "revision": "54b6f71e0c8f90e9dd8b",
+    "url": "/static/js/2.e16f2ba5.chunk.js"
+  },
+  {
+    "revision": "a19a1f3f6c6a9714acf0",
+    "url": "/static/js/main.b26104ce.chunk.js"
+  },
+  {
+    "revision": "536637ebfd4e1feea9be",
+    "url": "/static/js/runtime-main.93d7dfbd.js"
+  }
+]);
